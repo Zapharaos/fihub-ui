1aeb8acb0adbf79983b64315f11b89854b25ba72
Zapharaos <matthieu.freitag@gmail.com>
2024-12-03 22:16:42 +0100

    Restructure project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
-import {TransactionComponent} from "./transaction/transaction.component";
-import {TransactionsComponent} from "./transactions/transactions.component";
-import {ResetComponent} from "./reset/reset.component";
+import {LoginComponent} from "./modules/auth/pages/login/login.component";
+import {RegisterComponent} from "./modules/auth/pages/register/register.component";
+import {TransactionComponent} from "./modules/dashboard/transaction/transaction.component";
+import {TransactionsComponent} from "./modules/dashboard/transactions/transactions.component";
+import {ResetComponent} from "./modules/auth/pages/reset/reset.component";
 
 export const routes: Routes = [
   {
