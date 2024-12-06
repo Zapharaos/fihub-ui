b068f2dbc9d555a4e7f0793849809f9edf06e501
Zapharaos <matthieu.freitag@gmail.com>
2024-12-06 18:46:06 +0100

    API gen files update - Users authentication

--- src/app/core/api/api/api.ts
+++ src/app/core/api/api/api.ts
@@ -1,3 +1,5 @@
+export * from './auth.service';
+import { AuthService } from './auth.service';
 export * from './users.service';
 import { UsersService } from './users.service';
-export const APIS = [UsersService];
+export const APIS = [AuthService, UsersService];
