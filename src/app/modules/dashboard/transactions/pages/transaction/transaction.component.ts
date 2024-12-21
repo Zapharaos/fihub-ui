63178b383ca508a5c3cd8504f264369d8c9a54c4
Zapharaos <matthieu.freitag@gmail.com>
2024-12-21 21:59:31 +0100

    feat: transaction overview

--- src/app/modules/dashboard/transactions/pages/transaction/transaction.component.ts
+++ src/app/modules/dashboard/transactions/pages/transaction/transaction.component.ts
@@ -3,11 +3,16 @@ import {
   DashboardContentLayoutComponent
 } from "@modules/dashboard/layouts/dashboard-content-layout/dashboard-content-layout.component";
 import {ActivatedRoute, Router} from "@angular/router";
-import {TransactionsService, TransactionsTransaction} from "@core/api";
+import {BrokersBroker, TransactionsService, TransactionsTransaction} from "@core/api";
 import {Button} from "primeng/button";
 import {TransactionStore} from "@modules/dashboard/transactions/stores/transaction.service";
 import {finalize} from "rxjs";
 import {NotificationService} from "@shared/services/notification.service";
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'primeng/tabs';
+import {DialogMode} from "@shared/services/dialog.service";
+import {TranslatePipe} from "@ngx-translate/core";
+import {ConfirmService} from "@shared/services/confirm.service";
 
 @Component({
   selector: 'app-transaction',
@@ -15,6 +20,9 @@ import {NotificationService} from "@shared/services/notification.service";
   imports: [
     DashboardContentLayoutComponent,
     Button,
+    CommonModule,
+    TabsModule,
+    TranslatePipe,
   ],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.scss'
@@ -30,6 +38,7 @@ export class TransactionComponent implements OnInit {
     private transactionsService: TransactionsService,
     private transactionStore: TransactionStore,
     private notificationService: NotificationService,
+    private confirmService: ConfirmService,
   ) {}
 
   ngOnInit(): void {
@@ -70,4 +79,35 @@ export class TransactionComponent implements OnInit {
     this.transactionStore.transaction = this.transaction;
     this.router.navigate(['/dashboard/transactions', this.transaction?.id, 'update']);
   }
+
+  deleteTransaction(event: Event) {
+    this.confirmService.showDeleteConfirmation(event, () => {
+      this.transactionsService.deleteTransaction(this.transaction!.id!).pipe(finalize(() => {
+        this.loading = false;
+      })).subscribe({
+        next: () => {
+          this.notificationService.showToastSuccess('transactions.messages.delete-success');
+          this.router.navigate(['/dashboard/transactions']);
+        },
+        error: (error) => {
+          switch (error.status) {
+            case 400:
+              this.notificationService.showToastError('http.400.detail', undefined, 'http.400.summary')
+              break;
+            case 401:
+              this.notificationService.showToastError('http.401.detail', undefined, 'http.401.summary')
+              break;
+            case 404:
+              this.notificationService.showToastError('http.404.detail', undefined, 'http.404.summary')
+              break;
+            default:
+              this.notificationService.showToastError('http.500.detail', undefined, 'http.500.summary')
+              break;
+          }
+        }
+      })
+    })
+  }
+
+  protected readonly DialogMode = DialogMode;
 }
