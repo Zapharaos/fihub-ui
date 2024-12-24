675ea263d948b1098c57c2656433d2dca97f80f7
Zapharaos <matthieu.freitag@gmail.com>
2024-12-24 03:53:15 +0100

    feat: footer - privacy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {authGuard, noAuthGuard} from "@shared/guards/auth.guard";
 import {DashboardLayoutComponent} from "@modules/dashboard/layouts/dashboard-layout/dashboard-layout.component";
+import {TermsComponent} from "@core/pages/terms/terms.component";
+import {PrivacyComponent} from "@core/pages/privacy/privacy.component";
 
 export const routes: Routes = [
   {
@@ -23,5 +25,13 @@ export const routes: Routes = [
     canActivate: [authGuard],
     component: DashboardLayoutComponent,
     loadChildren: () => import('./modules/dashboard/dashboard.routes').then(m => m.routes),
+  },
+  {
+    path: 'terms',
+    component: TermsComponent,
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
   },
 ];
